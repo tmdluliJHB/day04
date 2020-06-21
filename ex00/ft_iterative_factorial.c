886deebd4592b057918650a6f391cb28152b38e4
Themba Mdluli <tmdluli@c4r6s6.wethinkcode.co.za>
2020-06-21 13:50:24 +0200

    added the error return value

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,15 +6,18 @@
 /*   By: tmdluli <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 12:56:12 by tmdluli           #+#    #+#             */
-/*   Updated: 2020/06/21 13:21:54 by tmdluli          ###   ########.fr       */
+/*   Updated: 2020/06/21 13:49:22 by tmdluli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */Wq
 int ft_iterative_factorial(int nb)
 {
 	int	fact = 1;
-
-	if(nb == 0 || nb == 1)
-		return 1;
+	
+	if(nb < 0 || nb > 12)
+		return 0;
+	else
+		if(nb == 0 || nb == 1)
+			return 1;
 
 	while(nb >= 1)
 	{
