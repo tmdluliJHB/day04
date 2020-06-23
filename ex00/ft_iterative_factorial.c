382962f2c1a410be5ff18de72a1e83172165bebc
Themba Mdluli <tmdluli@c4r6s6.wethinkcode.co.za>
2020-06-23 14:17:00 +0200

    edited the file

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -8,16 +8,16 @@
 /*   Created: 2020/06/21 12:56:12 by tmdluli           #+#    #+#             */
 /*   Updated: 2020/06/21 13:49:22 by tmdluli          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */Wq
+/* ************************************************************************** */
 int ft_iterative_factorial(int nb)
 {
-	int	fact = 1;
+	int fact = 1;
 	
 	if(nb < 0 || nb > 12)
-		return 0;
+		return (0);
 	else
 		if(nb == 0 || nb == 1)
-			return 1;
+			return (1);
 
 	while(nb >= 1)
 	{
@@ -25,6 +25,6 @@ int ft_iterative_factorial(int nb)
 		nb--;
 	}
 	
-	return fact;
+	return (fact);
 }
 
